853d9491dd18fb03509314da7ef58a6460a33fdd
stylkand <el17088@mail.ntua.gr>
2021-07-05 22:55:10 +0200

    add questinPerKeyword route

---
+++ MVC_APP/backend/app/routes/questionsPerKeyword.routes.js
@@ -0,0 +1,15 @@
+// const { authJwt } = require("../../middleware");
+const controller = require("../../controllers/public/questionsPerKeyword.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-observatory-auth, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+ app.get("/api/public/questionsPerKeyword",
+      controller.questionsPerKeyword);
+};
