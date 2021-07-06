b2434350be0d0e7861d94be5b347418e142fe072
stylkand <el17088@mail.ntua.gr>
2021-07-06 21:52:40 +0200

    remove: useless functions

--- MVC_APP/backend/app/controllers/public/questionsPerDay.controller.js
+++ MVC_APP/backend/app/controllers/public/questionsPerDay.controller.js
@@ -4,39 +4,6 @@ const sequelize = require("sequelize");
 
 
 
-function date_string_from_datetime(x) {
-  const temp = readable_datetime_string(x);
-  var date = '';
-  for (i = 0; i <= 9; i++)
-    date += temp[i];
-
-  return date;
-}
-
-function readable_datetime_string(x) {
- return x.toISOString().replace(/T/, ' ').replace(/\..+/, '');
-}
-
-function datetime_from_int(x, from_flag) {
-const temp = String(x);
-const year = temp[0] + temp[1] + temp[2] + temp[3];
-const month = temp[4] +  temp[5];
-const day = temp[6] + temp[7];
-var date = year + '-' + month + '-' + day;
-if (from_flag) {
-  date += ' 01:00:01';
-}
-else {
-  date += ' 23:59:59';
-}
-
-var datetime = new Date(date);
-datetime.setTime( datetime.getTime() - new Date().getTimezoneOffset()*60*1000 );
-return(new Date(datetime));
-}
-
-
-
 
 exports.questionsPerDay= (req, res) => {
   question.findAll({
