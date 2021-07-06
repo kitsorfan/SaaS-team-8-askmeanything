a99bc42860c18e70a475ef8b2a777278871698ca
kitsorfan <kitsorfan@protonmail.com>
2021-07-06 23:07:27 +0300

    fix answer userId

--- MVC_APP/backend/app/controllers/userCreate/newAnswer.controller.js
+++ MVC_APP/backend/app/controllers/userCreate/newAnswer.controller.js
@@ -4,8 +4,8 @@ const answer = db.answer;
 exports.newAnswer = (req, res) => {
     answer.create({
         atext: req.body.atext,
-        userID: req.userID,
-        questionID: req.body.questionID
+        userID: parseInt(req.userId),
+        questionID: parseInt(req.body.questionID)
       })
         .then(user => {
             res.send({ message: "Answer created succesfuly" });
