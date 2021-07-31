bb022fcd27866bfbbdfb1224e797ade7002f51ae
stylkand <el17088@mail.ntua.gr>
2021-07-31 11:22:57 +0200

    update sequelize structure for newQuestion

--- MVC_APP/backend/app/controllers/userCreate/newQuestion.controller.js
+++ MVC_APP/backend/app/controllers/userCreate/newQuestion.controller.js
@@ -5,10 +5,11 @@ exports.newQuestion = (req, res) => {
     question.create({
         title: req.body.title,
         qtext: req.body.qtext,
+        tagID: req.body.qtag,
         userID: parseInt(req.userId)
       })
         .then(user => {
-            res.send({ message: "Question created succesfuly" });
+            res.status(201).send({ message: "Question created succesfuly" });
         })
         .catch(err => {
           res.status(500).send({ message: err.message });
